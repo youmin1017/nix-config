9eefa29e6ffcc8f4247c8d96c953e73e807ae3a1
youmin1017 <youmin.main@gmail.com>
2025-06-03 11:53:19 +0800

    feat(nvim): rename mapping

--- hosts/nixos/modules/system.nix
+++ hosts/nixos/modules/system.nix
@@ -44,6 +44,18 @@
       fcitx5-rime
       rime-data
     ];
+
+    fcitx5.settings = {
+      inputMethod = {
+        "Groups/0" = {
+          Name = "Default";
+          "Default Layout" = "us";
+          DefaultIM = "keyboard-us";
+        };
+        "Groups/0/Items/0".Name = "keyboard-us";
+        "Groups/0/Items/1".Name = "rime";
+      };
+    };
   };
 
   fonts.packages = with pkgs; [
