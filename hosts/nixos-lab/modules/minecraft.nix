be0f2328468bed54f1534224c5c5dea89a642ee5
youmin1017 <youmin.main@gmail.com>
2025-06-25 14:53:13 +0800

    subtree: test

---
+++ hosts/nixos-lab/modules/minecraft.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+{
+  services.minecraft-server = {
+    package = pkgs.papermcServers.papermc-1_21_5;
+    enable = true;
+    eula = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    prismlauncher
+  ];
+}
