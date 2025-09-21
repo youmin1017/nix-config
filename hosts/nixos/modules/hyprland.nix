355614d565a8c00551f4a8b7239e809971e6fd97
youmin1017 <youmin.main@gmail.com>
2025-09-21 19:05:10 +0800

    feat: Hyprland and related

--- hosts/nixos/modules/hyprland.nix
+++ hosts/nixos/modules/hyprland.nix
@@ -1,14 +1,23 @@
 { pkgs, ... }:
 {
-  programs.hyprland.enable = true;
-  # Optional, hint electron apps to use wayland:
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  services.displayManager.gdm.enable = true;
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+  };
+
+  # Extensions
+  services.hypridle.enable = true;
+  programs.hyprlock.enable = true;
+  programs.nm-applet.enable = true;
+  programs.waybar.enable = true;
 
   environment.systemPackages = with pkgs; [
-    networkmanagerapplet
-    waybar
-    wlogout
-    swww
     rofi
+    swww
+    wlogout
   ];
+
+  # Optional, hint electron apps to use wayland:
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
