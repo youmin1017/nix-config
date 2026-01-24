db165a26a69ad23ea6463de100bf50e56121a96f
youmin1017 <youmin.main@gmail.com>
2026-01-24 23:49:25 +0800

    Update utils programs

--- modules/home/programs/utils/default.nix
+++ modules/home/programs/utils/default.nix
@@ -11,24 +11,25 @@
     home.packages = with pkgs; [
       # utils
       age # A simple, modern and secure encryption tool
-      just # A handy way to save and run project-specific commands
       axel # A light download accelerator
-      socat # replacement of openbsd-netcat
-      nmap # A utility for network discovery and security auditing
-      unzip # A utility for unpacking zip file
-      unar # A program to extract, list, test and view the contents of archives
-      ripgrep # A line-oriented search tool that recursively searches your current directory for a regex pattern
-      yq-go # yaml processer https://github.com/mikefarah/yq
+      btop # A customizable resource monitor (CPU, memory, disks, network)
+      hey # A tiny program for HTTP benchmarking and load testing
+      inkscape # A professional vector graphics editor and utility
+      jless # A command-line JSON viewer and pager for interactive exploration
       jq # A lightweight and flexible command-line JSON processor
-      inkscape # vector graphics utility
-      jless
-      btop
-      openssl
-      mkcert
-      termscp
-      pwgen
-      lsof
-      lazygit
+      just # A handy way to save and run project-specific commands
+      lazygit # A simple terminal UI for git commands
+      lsof # A utility that lists information about files opened by processes
+      mkcert # A simple tool for making locally-trusted development certificates
+      nmap # A utility for network discovery and security auditing
+      openssl # A robust toolkit for TLS and general-purpose cryptography
+      pwgen # A password generator that creates easy-to-memorize, secure passwords
+      ripgrep # A fast, line-oriented search tool (recursive regex search)
+      socat # A multipurpose relay (bidirectional data transfer) and netcat replacement
+      termscp # A terminal file explorer with support for SCP, SFTP, FTP, and S3
+      unar # A program to extract, list, and view contents of various archive formats
+      unzip # A utility for unpacking .zip archives
+      yq-go # A portable command-line YAML processor
 
       # misc
       onefetch # Git repository summary generator
