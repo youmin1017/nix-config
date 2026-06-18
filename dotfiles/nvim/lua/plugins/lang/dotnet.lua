f453eca7a2f33b766df600c1f3e9dce9eef2e131
youmin1017 <youmin.main@gmail.com>
2026-06-19 00:14:50 +0800

    feat(nvim): disable dotnet codelens

--- dotfiles/nvim/lua/plugins/lang/dotnet.lua
+++ dotfiles/nvim/lua/plugins/lang/dotnet.lua
@@ -1,11 +1,15 @@
+---@diagnostic disable: missing-fields
 ---@type LazySpec
 return {
   {
     "GustavEikaas/easy-dotnet.nvim",
     ---@type easy-dotnet.Options
-    ---@diagnostic disable-next-line: missing-fields
     opts = {
       picker = "snacks",
+      lsp = {
+        enabled = true,
+        auto_refresh_codelens = false,
+      },
     },
   },
 }
